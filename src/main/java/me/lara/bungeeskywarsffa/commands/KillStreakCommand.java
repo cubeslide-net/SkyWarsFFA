d1e052b51b78eb779cbbaf0ebef9aee17550eadd
=Timeox2k <schockilp@gmail.com>
2022-09-15 13:22:46 +0200

    Remade Killstreak-Command

--- src/main/java/me/lara/bungeeskywarsffa/commands/KillStreakCommand.java
+++ src/main/java/me/lara/bungeeskywarsffa/commands/KillStreakCommand.java
@@ -1,6 +1,7 @@
 package me.lara.bungeeskywarsffa.commands;
 
 import java.util.UUID;
+
 import me.lara.bungeeskywarsffa.BungeeSkywarsFFA;
 import me.lara.bungeeskywarsffa.listeners.PlayerListeners;
 import org.bukkit.command.Command;
@@ -10,25 +11,26 @@ import org.bukkit.entity.Player;
 
 public class KillStreakCommand implements CommandExecutor {
 
-  @Override
-  public boolean onCommand(CommandSender commandSender, Command command, String s,
-      String[] strings) {
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s,
+                             String[] strings) {
 
-    if (!(commandSender instanceof Player)) {
-      commandSender.sendMessage("This Command only works for Players.");
-      return true;
+        if (!(commandSender instanceof Player)) {
+            commandSender.sendMessage("This Command only works for Players.");
+            return true;
+        }
+
+        final Player player = (Player) commandSender;
+        final UUID uuid = player.getUniqueId();
+
+        if (!PlayerListeners.killStreakCount.containsKey(uuid)) {
+            player.sendMessage(BungeeSkywarsFFA.getPrefix() + "§7Your current KillStreak§8: §e0");
+            return true;
+        }
+
+        player.sendMessage(BungeeSkywarsFFA.getPrefix() + "§7Your current KillStreak§8: §e"
+                + PlayerListeners.killStreakCount.get(uuid));
+
+        return true;
     }
-
-    final Player player = (Player) commandSender;
-    final UUID uuid = player.getUniqueId();
-
-    if (PlayerListeners.killStreakCount.containsKey(uuid)) {
-      player.sendMessage(BungeeSkywarsFFA.getPrefix() + "§7Your current KillStreak§8: §e"
-          + PlayerListeners.killStreakCount.get(uuid));
-    } else {
-      player.sendMessage(BungeeSkywarsFFA.getPrefix() + "§7Your current KillStreak§8: §e0");
-    }
-
-    return true;
-  }
 }
