c11a1c67f65817d5c8ebd3f483d23d75168724ff
Timeox2k <schockilp@gmail.com>
2022-10-05 00:34:09 +0200

    Recoded /stats Command

--- src/main/java/me/lara/bungeeskywarsffa/commands/CommandStats.java
+++ src/main/java/me/lara/bungeeskywarsffa/commands/CommandStats.java
@@ -1,9 +1,5 @@
 package me.lara.bungeeskywarsffa.commands;
 
-import java.text.DecimalFormat;
-import java.util.List;
-import java.util.Objects;
-import java.util.UUID;
 import me.lara.bungeeskywarsffa.BungeeSkywarsFFA;
 import me.lara.bungeeskywarsffa.utils.Database;
 import org.bukkit.Bukkit;
@@ -13,58 +9,57 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import java.text.DecimalFormat;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+
 public class CommandStats implements CommandExecutor {
 
-  @Override
-  public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
-    final Database database = BungeeSkywarsFFA.getInstance().getDatabase();
+        final Database database = BungeeSkywarsFFA.getInstance().getDatabase();
 
-    final Player player = (Player) sender;
+        final Player player = (Player) sender;
 
-    final DecimalFormat decimalFormat = new DecimalFormat("#.##");
-    final List<String> messages = BungeeSkywarsFFA.getInstance().getConfig()
-        .getStringList("Messages.stats");
+        final DecimalFormat decimalFormat = new DecimalFormat("#.##");
+        final List<String> messages = BungeeSkywarsFFA.getInstance().getConfig()
+                .getStringList("Messages.stats");
 
-    if (args.length == 0) {
-      final UUID uuid = player.getUniqueId();
-      final double kills = database.getKills(uuid);
-      final double deaths = database.getDeaths(uuid);
 
-      double kdr = kills / deaths;
+        UUID uuid;
+        double kills;
+        double deaths;
+        String name;
 
-      for (String line : messages) {
-        String message = line.replace("&", "§").replace("%player%", player.getName())
-            .replace("%kills%", String.valueOf((int) kills)).replace("%deaths%", String.valueOf((int) deaths))
-            .replace("%kd%", decimalFormat.format(kdr)).replace("%rank%", String.valueOf(database.getRank(uuid)));
+        if (args.length == 0) {
+            uuid = player.getUniqueId();
+            kills = database.getKills(uuid);
+            deaths = database.getDeaths(uuid);
+            name = player.getName();
+        } else {
+            OfflinePlayer target = Bukkit.getOfflinePlayer(args[0]);
+            if (!database.doesPlayerExistByUUID(target.getUniqueId())) {
+                player.sendMessage(BungeeSkywarsFFA.getPrefix() + "§cThis Player has not Played yet!");
+                return true;
+            }
+            uuid = target.getUniqueId();
+            kills = database.getKills(uuid);
+            deaths = database.getDeaths(uuid);
+            name = target.getName();
+        }
 
-        player.sendMessage(message);
-      }
-    } else {
+        double kdr = kills / deaths;
 
-      OfflinePlayer target = Bukkit.getOfflinePlayer(args[0]);
+        for (String line : messages) {
+            String message = line.replace("&", "§")
+                    .replace("%player%", Objects.requireNonNull(name))
+                    .replace("%kills%", String.valueOf((int) kills)).replace("%deaths%", String.valueOf((int) deaths))
+                    .replace("%kd%", decimalFormat.format(kdr)).replace("%rank%", String.valueOf(database.getRank(uuid)));
+            player.sendMessage(message);
 
-      if (!database.doesPlayerExistByUUID(target.getUniqueId())) {
-        player.sendMessage(BungeeSkywarsFFA.getPrefix() + "§cThis Player has not Played yet!");
+        }
         return true;
-      }
-
-      final UUID targetUUID = target.getUniqueId();
-      final double kills = database.getKills(targetUUID);
-      final double deaths = database.getDeaths(targetUUID);
-
-      double kdr = kills / deaths;
-
-      for (String line : messages) {
-        String message = line.replace("&", "§")
-            .replace("%player%", Objects.requireNonNull(target.getName()))
-            .replace("%kills%", String.valueOf(kills)).replace("%deaths%", String.valueOf(deaths))
-            .replace("%kd%", decimalFormat.format(kdr)).replace("%rank%", String.valueOf(database.getRank(targetUUID)));
-        player.sendMessage(message);
-      }
-
     }
-
-    return true;
-  }
 }
